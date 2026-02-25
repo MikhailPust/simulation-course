38c63ece8e65a8d20adee564f76255456fdfb6e5
Mikhail Pustovoi <mpust2233@gmail.com>
2026-02-26 02:26:32 +0700

    lab2

---
+++ lab02/lab2/lab2/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace lab2
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.inputL = new System.Windows.Forms.NumericUpDown();
+            this.inputTLeft = new System.Windows.Forms.NumericUpDown();
+            this.inputTRight = new System.Windows.Forms.NumericUpDown();
+            this.inputT0 = new System.Windows.Forms.NumericUpDown();
+            this.inputTau = new System.Windows.Forms.NumericUpDown();
+            this.inputH = new System.Windows.Forms.NumericUpDown();
+            this.btStart = new System.Windows.Forms.Button();
+            this.btClear = new System.Windows.Forms.Button();
+            this.labCenter = new System.Windows.Forms.Label();
+            this.labTime = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputL)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputTLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputTRight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputT0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputTau)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputH)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea5);
+            this.chart1.Location = new System.Drawing.Point(220, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(650, 380);
+            this.chart1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeight = 29;
+            this.dataGridView1.Location = new System.Drawing.Point(220, 400);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(650, 160);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // inputL
+            // 
+            this.inputL.DecimalPlaces = 3;
+            this.inputL.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.inputL.Location = new System.Drawing.Point(12, 38);
+            this.inputL.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.inputL.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            196608});
+            this.inputL.Name = "inputL";
+            this.inputL.Size = new System.Drawing.Size(100, 22);
+            this.inputL.TabIndex = 3;
+            this.inputL.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            196608});
+            // 
+            // inputTLeft
+            // 
+            this.inputTLeft.Location = new System.Drawing.Point(12, 93);
+            this.inputTLeft.Maximum = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            this.inputTLeft.Minimum = new decimal(new int[] {
+            200,
+            0,
+            0,
+            -2147483648});
+            this.inputTLeft.Name = "inputTLeft";
+            this.inputTLeft.Size = new System.Drawing.Size(100, 22);
+            this.inputTLeft.TabIndex = 5;
+            // 
+            // inputTRight
+            // 
+            this.inputTRight.Location = new System.Drawing.Point(12, 148);
+            this.inputTRight.Maximum = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            this.inputTRight.Minimum = new decimal(new int[] {
+            200,
+            0,
+            0,
+            -2147483648});
+            this.inputTRight.Name = "inputTRight";
+            this.inputTRight.Size = new System.Drawing.Size(100, 22);
+            this.inputTRight.TabIndex = 7;
+            // 
+            // inputT0
+            // 
+            this.inputT0.Location = new System.Drawing.Point(12, 203);
+            this.inputT0.Maximum = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            this.inputT0.Minimum = new decimal(new int[] {
+            200,
+            0,
+            0,
+            -2147483648});
+            this.inputT0.Name = "inputT0";
+            this.inputT0.Size = new System.Drawing.Size(100, 22);
+            this.inputT0.TabIndex = 9;
+            // 
+            // inputTau
+            // 
+            this.inputTau.Location = new System.Drawing.Point(12, 258);
+            this.inputTau.Name = "inputTau";
+            this.inputTau.Size = new System.Drawing.Size(100, 22);
+            this.inputTau.TabIndex = 11;
+            // 
+            // inputH
+            // 
+            this.inputH.Location = new System.Drawing.Point(12, 313);
+            this.inputH.Name = "inputH";
+            this.inputH.Size = new System.Drawing.Size(100, 22);
+            this.inputH.TabIndex = 13;
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(12, 355);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(100, 30);
+            this.btStart.TabIndex = 14;
+            this.btStart.Text = "Запуск";
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // btClear
+            // 
+            this.btClear.Location = new System.Drawing.Point(12, 395);
+            this.btClear.Name = "btClear";
+            this.btClear.Size = new System.Drawing.Size(100, 30);
+            this.btClear.TabIndex = 15;
+            this.btClear.Text = "Очистить";
+            this.btClear.Click += new System.EventHandler(this.btClear_Click);
+            // 
+            // labCenter
+            // 
+            this.labCenter.Location = new System.Drawing.Point(12, 440);
+            this.labCenter.Name = "labCenter";
+            this.labCenter.Size = new System.Drawing.Size(200, 20);
+            this.labCenter.TabIndex = 16;
+            this.labCenter.Text = "T в центре: —";
+            // 
+            // labTime
+            // 
+            this.labTime.Location = new System.Drawing.Point(12, 465);
+            this.labTime.Name = "labTime";
+            this.labTime.Size = new System.Drawing.Size(200, 20);
+            this.labTime.TabIndex = 17;
+            this.labTime.Text = "Реальное время счёта: —";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Толщина L (м):";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Тем-ра слева Tл (°C):";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(12, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Тем-ра справа Tп (°C):";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(12, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Нач. температура T0 (°C):";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(12, 235);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(145, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Шаг по времени τ (с):";
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(12, 290);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(145, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Шаг по простр. h (м):";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(651, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(176, 32);
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(890, 580);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputL);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.inputTLeft);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.inputTRight);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.inputT0);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.inputTau);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.inputH);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.btClear);
+            this.Controls.Add(this.labCenter);
+            this.Controls.Add(this.labTime);
+            this.Name = "Form1";
+            this.Text = "Теплопроводность — Алюминий";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputL)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputTLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputTRight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputT0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputTau)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputH)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.NumericUpDown inputL;
+        private System.Windows.Forms.NumericUpDown inputTLeft;
+        private System.Windows.Forms.NumericUpDown inputTRight;
+        private System.Windows.Forms.NumericUpDown inputT0;
+        private System.Windows.Forms.NumericUpDown inputTau;
+        private System.Windows.Forms.NumericUpDown inputH;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Button btClear;
+        private System.Windows.Forms.Label labCenter;
+        private System.Windows.Forms.Label labTime;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ lab02/lab2/lab2/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Diagnostics;
+
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Drawing;
+
+namespace lab2
+{
+    public partial class Form1 : Form
+    {
+        // ПАРАМЕТРЫ АЛЮМИНИЯ
+        const double rho = 2700;    // плотность, кг/м³
+        const double c = 900;       // теплоёмкость, Дж/(кг·°C)
+        const double lambda = 237;  // теплопроводность, Вт/(м·°C)
+
+        // ПАРАМЕТРЫ ПЛАСТИНЫ
+        double L;   // толщина пластины, м
+        double TL;  // температура левой границы, °C
+        double TR;  // температура правой границы, °C
+        double T0;  // начальная температура внутри, °C
+
+
+        double tau; // шаг по времени, с
+        double h;   // шаг по пространству, м
+
+
+        double[] T;     // температура в каждой точке пластины
+        double[] alpha; // коэффициенты прямой прогонки α
+        double[] beta;  // коэффициенты прямой прогонки β
+
+        int N;          // количество интервалов по пространству
+        double modelTime;  // текущее модельное время, с
+        double targetTime; // целевое время (2 с)
+        Stopwatch realTimer;  // замер реального времени счёта
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetupChart();
+            SetupGrid();
+            SetDefaults();
+        }
+
+        private void SetupChart()
+        {
+            chart1.Series.Clear();
+            chart1.ChartAreas[0].AxisX.Title = "x, м";
+            chart1.ChartAreas[0].AxisY.Title = "T, °C";
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            chart1.ChartAreas[0].AxisX.MajorGrid.LineColor =
+                System.Drawing.Color.LightGray;
+            chart1.ChartAreas[0].AxisY.MajorGrid.LineColor =
+                System.Drawing.Color.LightGray;
+        }
+
+        private void SetupGrid()
+        {
+            dataGridView1.Columns.Clear();
+            dataGridView1.Columns.Add("tau", "Шаг по времени τ, с");
+            dataGridView1.Columns.Add("h", "Шаг по х h, м");
+            dataGridView1.Columns.Add("Tcenter", "T в центре, °C");
+            dataGridView1.Columns.Add("realT", "Реал. время счёта, с");
+
+            foreach (DataGridViewColumn col in dataGridView1.Columns)
+                col.Width = 170;
+
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.AllowUserToAddRows = false;
+        }
+
+        private void SetDefaults()
+        {
+            inputL.Value = (decimal)0.1;
+            inputTLeft.Value = 200;
+            inputTRight.Value = 20;
+            inputT0.Value = 20;
+
+            inputTau.Value = (decimal)0.01;
+            inputTau.DecimalPlaces = 4;
+            inputTau.Increment = (decimal)0.001;
+            inputTau.Minimum = (decimal)0.0001;
+            inputTau.Maximum = (decimal)1;
+
+            inputH.Value = (decimal)0.01;
+            inputH.DecimalPlaces = 4;
+            inputH.Increment = (decimal)0.001;
+            inputH.Minimum = (decimal)0.0001;
+            inputH.Maximum = (decimal)1;
+        }
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Stop();
+                realTimer.Stop();
+            }
+
+            L = (double)inputL.Value;
+            TL = (double)inputTLeft.Value;
+            TR = (double)inputTRight.Value;
+            T0 = (double)inputT0.Value;
+            tau = (double)inputTau.Value;
+            h = (double)inputH.Value;
+
+            N = (int)(L / h);
+            h = L / N;
+
+            T = new double[N + 1];
+            alpha = new double[N + 1];
+            beta = new double[N + 1];
+
+            for (int i = 0; i <= N; i++)
+                T[i] = T0;
+
+            T[0] = TL; 
+            T[N] = TR; 
+
+            modelTime = 0;
+            targetTime = 2.0;
+
+            realTimer = Stopwatch.StartNew();
+
+        
+            timer1.Interval = 1;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int stepsPerTick = 50;
+
+            for (int step = 0; step < stepsPerTick; step++)
+            {
+                if (modelTime >= targetTime) break;
+                StepForward();
+                modelTime += tau;
+            }
+
+            labCenter.Text = string.Format(
+                "T в центре: {0:F4} °C   |   t = {1:F4} с",
+                T[N / 2], modelTime);
+                DrawHeatStrip();
+
+            if (modelTime >= targetTime)
+            {
+                timer1.Stop();
+                realTimer.Stop();
+
+                DrawTemperature(string.Format(
+                    "τ={0} h={1}",
+                    tau.ToString("F4"),
+                    h.ToString("F4")));
+
+                double Tcenter = T[N / 2];
+
+                double realSec = realTimer.Elapsed.TotalSeconds;
+
+                labTime.Text = string.Format(
+                    "Реальное время счёта: {0:F3} с", realSec);
+
+                dataGridView1.Rows.Add(
+                    tau.ToString("F4"),
+                    h.ToString("F4"),
+                    Tcenter.ToString("F4"),
+                    realSec.ToString("F3")
+                );
+            }
+        }
+
+        private void StepForward()
+        {
+            double A = lambda / (h * h);
+            double B = 2.0 * lambda / (h * h) + rho * c / tau;
+            double C = lambda / (h * h);
+
+            alpha[1] = 0;
+            beta[1] = TL;
+
+            for (int i = 1; i <= N - 1; i++)
+            {
+                double D = (rho * c / tau) * T[i];
+
+                double denom = B - A * alpha[i];
+
+                alpha[i + 1] = C / denom;
+                beta[i + 1] = (D + A * beta[i]) / denom;
+            }
+
+            T[N] = TR;
+
+            for (int i = N - 1; i >= 1; i--)
+                T[i] = alpha[i + 1] * T[i + 1] + beta[i + 1];
+
+            T[0] = TL;
+            T[N] = TR;
+        }
+
+
+        private void DrawTemperature(string name)
+        {
+            if (chart1.Series.IndexOf(name) >= 0)
+                chart1.Series.Remove(chart1.Series[name]);
+
+            var series = new Series
+            {
+                ChartType = SeriesChartType.Line,
+                BorderWidth = 2,
+                Name = name
+            };
+
+            for (int i = 0; i <= N; i++)
+                series.Points.AddXY(i * h, T[i]);
+
+            chart1.Series.Add(series);
+        }
+
+        private Color GetTemperatureColor(double temp)
+        {
+            double min = (double)inputT0.Value;
+            double max = Math.Max((double)inputTLeft.Value, (double)inputTRight.Value);
+
+            if (Math.Abs(max - min) < 0.01) max = min + 1;
+
+            double ratio = (temp - min) / (max - min);
+            if (ratio < 0) ratio = 0;
+            if (ratio > 1) ratio = 1;
+
+            int r = (int)Math.Max(0, 255 * (2 * ratio - 1));
+            int b = (int)Math.Max(0, 255 * (1 - 2 * ratio));
+            int g = 255 - r - b;
+
+            return Color.FromArgb(r, g, b);
+        }
+
+        private void DrawHeatStrip()
+        {
+            if (pictureBox1 == null || pictureBox1.Width <= 0 || T == null) return;
+
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                double stepWidth = (double)pictureBox1.Width / (N + 1);
+                for (int i = 0; i <= N; i++)
+                {
+                    Color c = GetTemperatureColor(T[i]);
+                    using (Brush brush = new SolidBrush(c))
+                    {
+                        g.FillRectangle(brush, (float)(i * stepWidth), 0, (float)stepWidth + 1, pictureBox1.Height);
+                    }
+                }
+            }
+
+            if (pictureBox1.Image != null) pictureBox1.Image.Dispose();
+            pictureBox1.Image = bmp;
+        }
+        private void btClear_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            if (realTimer != null) realTimer.Stop();
+            chart1.Series.Clear();
+            dataGridView1.Rows.Clear();
+            labCenter.Text = "T в центре: —";
+            labTime.Text = "Реальное время счёта: —";
+            if (pictureBox1.Image != null)
+            {
+                pictureBox1.Image.Dispose(); 
+                pictureBox1.Image = null;    
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
