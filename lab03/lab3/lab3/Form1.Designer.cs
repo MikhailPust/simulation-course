2960cc562443685711ee64c9913db7280f0765a8
Mikhail Pustovoi <mpust2233@gmail.com>
2026-03-12 03:14:01 +0700

    lab3

---
+++ lab03/lab3/lab3/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace lab3
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btStart = new System.Windows.Forms.Button();
+            this.btStep = new System.Windows.Forms.Button();
+            this.btReset = new System.Windows.Forms.Button();
+            this.trackSpeed = new System.Windows.Forms.TrackBar();
+            this.trackDensity = new System.Windows.Forms.TrackBar();
+            this.trackGrow = new System.Windows.Forms.TrackBar();
+            this.trackFire = new System.Windows.Forms.TrackBar();
+            this.trackWater = new System.Windows.Forms.TrackBar();
+            this.trackWind = new System.Windows.Forms.TrackBar();
+            this.labStats = new System.Windows.Forms.Label();
+            this.labSpeed = new System.Windows.Forms.Label();
+            this.labBasic = new System.Windows.Forms.Label();
+            this.labExtra = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackSpeed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackDensity)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackGrow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackFire)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackWater)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackWind)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.SandyBrown;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(668, 12);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(120, 32);
+            this.btStart.TabIndex = 1;
+            this.btStart.Text = "Старт";
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // btStep
+            // 
+            this.btStep.Location = new System.Drawing.Point(668, 52);
+            this.btStep.Name = "btStep";
+            this.btStep.Size = new System.Drawing.Size(120, 32);
+            this.btStep.TabIndex = 2;
+            this.btStep.Text = "Шаг";
+            this.btStep.Click += new System.EventHandler(this.btStep_Click);
+            // 
+            // btReset
+            // 
+            this.btReset.Location = new System.Drawing.Point(668, 92);
+            this.btReset.Name = "btReset";
+            this.btReset.Size = new System.Drawing.Size(120, 32);
+            this.btReset.TabIndex = 3;
+            this.btReset.Text = "Сброс";
+            this.btReset.Click += new System.EventHandler(this.btReset_Click);
+            // 
+            // trackSpeed
+            // 
+            this.trackSpeed.Location = new System.Drawing.Point(663, 153);
+            this.trackSpeed.Maximum = 1000;
+            this.trackSpeed.Minimum = 50;
+            this.trackSpeed.Name = "trackSpeed";
+            this.trackSpeed.Size = new System.Drawing.Size(130, 56);
+            this.trackSpeed.TabIndex = 5;
+            this.trackSpeed.TickFrequency = 100;
+            this.trackSpeed.Value = 100;
+            this.trackSpeed.Scroll += new System.EventHandler(this.trackSpeed_Scroll);
+            // 
+            // trackDensity
+            // 
+            this.trackDensity.Location = new System.Drawing.Point(663, 246);
+            this.trackDensity.Maximum = 90;
+            this.trackDensity.Minimum = 10;
+            this.trackDensity.Name = "trackDensity";
+            this.trackDensity.Size = new System.Drawing.Size(130, 56);
+            this.trackDensity.TabIndex = 8;
+            this.trackDensity.TickFrequency = 10;
+            this.trackDensity.Value = 60;
+            // 
+            // trackGrow
+            // 
+            this.trackGrow.Location = new System.Drawing.Point(663, 316);
+            this.trackGrow.Maximum = 20;
+            this.trackGrow.Name = "trackGrow";
+            this.trackGrow.Size = new System.Drawing.Size(130, 56);
+            this.trackGrow.TabIndex = 10;
+            this.trackGrow.TickFrequency = 2;
+            this.trackGrow.Value = 5;
+            // 
+            // trackFire
+            // 
+            this.trackFire.Location = new System.Drawing.Point(663, 411);
+            this.trackFire.Name = "trackFire";
+            this.trackFire.Size = new System.Drawing.Size(130, 56);
+            this.trackFire.TabIndex = 13;
+            this.trackFire.Value = 1;
+            // 
+            // trackWater
+            // 
+            this.trackWater.Location = new System.Drawing.Point(663, 481);
+            this.trackWater.Name = "trackWater";
+            this.trackWater.Size = new System.Drawing.Size(130, 56);
+            this.trackWater.TabIndex = 15;
+            this.trackWater.Value = 3;
+            // 
+            // trackWind
+            // 
+            this.trackWind.Location = new System.Drawing.Point(663, 551);
+            this.trackWind.Maximum = 90;
+            this.trackWind.Name = "trackWind";
+            this.trackWind.Size = new System.Drawing.Size(130, 56);
+            this.trackWind.TabIndex = 17;
+            this.trackWind.TickFrequency = 10;
+            // 
+            // labStats
+            // 
+            this.labStats.Font = new System.Drawing.Font("Consolas", 9F);
+            this.labStats.Location = new System.Drawing.Point(12, 520);
+            this.labStats.Name = "labStats";
+            this.labStats.Size = new System.Drawing.Size(640, 20);
+            this.labStats.TabIndex = 19;
+            this.labStats.Text = "Деревьев: —    Горит: —    Сгорело: —    Вода: —    Пусто: —";
+            // 
+            // labSpeed
+            // 
+            this.labSpeed.Location = new System.Drawing.Point(668, 135);
+            this.labSpeed.Name = "labSpeed";
+            this.labSpeed.Size = new System.Drawing.Size(140, 18);
+            this.labSpeed.TabIndex = 4;
+            this.labSpeed.Text = "Скорость: 100 мс";
+            // 
+            // labBasic
+            // 
+            this.labBasic.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Bold);
+            this.labBasic.ForeColor = System.Drawing.Color.DarkGreen;
+            this.labBasic.Location = new System.Drawing.Point(663, 205);
+            this.labBasic.Name = "labBasic";
+            this.labBasic.Size = new System.Drawing.Size(145, 18);
+            this.labBasic.TabIndex = 6;
+            this.labBasic.Text = "── ОСНОВНЫЕ ПРАВИЛА ──";
+            // 
+            // labExtra
+            // 
+            this.labExtra.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Bold);
+            this.labExtra.ForeColor = System.Drawing.Color.DarkRed;
+            this.labExtra.Location = new System.Drawing.Point(663, 370);
+            this.labExtra.Name = "labExtra";
+            this.labExtra.Size = new System.Drawing.Size(145, 18);
+            this.labExtra.TabIndex = 11;
+            this.labExtra.Text = "── ДОППР. ПРАВИЛА ──";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(668, 228);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 18);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Плотность леса (%):";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(668, 298);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 18);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Рост деревьев (x0.001):";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(668, 393);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 18);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Молния (x0.001):";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(668, 463);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 18);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Водоёмы (кол-во):";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(668, 533);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(140, 18);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Ветер вправо (%):";
+            // 
+            // label6
+            // 
+            this.label6.ForeColor = System.Drawing.Color.Gray;
+            this.label6.Location = new System.Drawing.Point(12, 500);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(340, 18);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "ЛКМ — огонь   ПКМ — вода   СКМ — дерево";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(940, 682);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.btStep);
+            this.Controls.Add(this.btReset);
+            this.Controls.Add(this.labSpeed);
+            this.Controls.Add(this.trackSpeed);
+            this.Controls.Add(this.labBasic);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackDensity);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackGrow);
+            this.Controls.Add(this.labExtra);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackFire);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackWater);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.trackWind);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.labStats);
+            this.Name = "Form1";
+            this.Text = "Лабораторная 3 — Лесные пожары";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackSpeed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackDensity)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackGrow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackFire)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackWater)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackWind)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Button btStep;
+        private System.Windows.Forms.Button btReset;
+        private System.Windows.Forms.TrackBar trackSpeed;
+        private System.Windows.Forms.TrackBar trackDensity;
+        private System.Windows.Forms.TrackBar trackGrow;
+        private System.Windows.Forms.TrackBar trackFire;
+        private System.Windows.Forms.TrackBar trackWater;
+        private System.Windows.Forms.TrackBar trackWind;
+        private System.Windows.Forms.Label labStats;
+        private System.Windows.Forms.Label labSpeed;
+        private System.Windows.Forms.Label labBasic;
+        private System.Windows.Forms.Label labExtra;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ lab03/lab3/lab3/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace lab3
+{
+    public partial class Form1 : Form
+    {
+        const int EMPTY = 0;
+        const int TREE = 1;
+        const int BURNING = 2;
+        const int BURNED = 3;
+        const int WATER = 4;
+
+        static readonly Color COLOR_EMPTY = Color.SandyBrown;
+        static readonly Color COLOR_TREE = Color.ForestGreen;
+        static readonly Color COLOR_BURNING = Color.OrangeRed;
+        static readonly Color COLOR_BURNED = Color.DimGray;
+        static readonly Color COLOR_WATER = Color.DodgerBlue;
+
+        int rows, cols;
+        const int CELL = 8;
+
+        int[,] grid;
+        int[,] newGrid;
+
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitGrid();
+        }
+
+        private void InitGrid()
+        {
+            cols = pictureBox1.Width / CELL;
+            rows = pictureBox1.Height / CELL;
+
+            grid = new int[rows, cols];
+            newGrid = new int[rows, cols];
+
+            double density = trackDensity.Value / 100.0;
+
+            for (int r = 0; r < rows; r++)
+                for (int c = 0; c < cols; c++)
+                    grid[r, c] = (rnd.NextDouble() < density) ? TREE : EMPTY;
+
+            GenerateWater();
+
+            grid[rows / 2, cols / 2] = BURNING;
+
+            DrawGrid();
+            UpdateStats();
+        }
+
+        private void GenerateWater()
+        {
+            for (int w = 0; w < trackWater.Value; w++)
+            {
+                if (rnd.NextDouble() < 0.5) GenerateRiver();
+                else GenerateLake();
+            }
+        }
+
+        private void GenerateRiver()
+        {
+            bool horizontal = rnd.NextDouble() < 0.5;
+            int r = horizontal ? rnd.Next(rows) : 0;
+            int c = horizontal ? 0 : rnd.Next(cols);
+            int width = rnd.Next(2, 4);
+
+            for (int step = 0; step < (horizontal ? cols : rows); step++)
+            {
+                if (rnd.NextDouble() < 0.3)
+                {
+                    if (horizontal) r += rnd.Next(-1, 2);
+                    else c += rnd.Next(-1, 2);
+                }
+
+                for (int i = -width / 2; i <= width / 2; i++)
+                {
+                    int wr = horizontal ? r + i : r;
+                    int wc = horizontal ? c : c + i;
+                    if (wr >= 0 && wr < rows && wc >= 0 && wc < cols)
+                        grid[wr, wc] = WATER;
+                }
+
+                if (horizontal) c++;
+                else r++;
+            }
+        }
+
+        private void GenerateLake()
+        {
+            int cr = rnd.Next(rows);
+            int cc = rnd.Next(cols);
+            int radius = rnd.Next(3, 9);
+
+            for (int r = cr - radius; r <= cr + radius; r++)
+                for (int c = cc - radius; c <= cc + radius; c++)
+                {
+                    if (r < 0 || r >= rows || c < 0 || c >= cols) continue;
+                    double dist = Math.Sqrt((r - cr) * (r - cr) + (c - cc) * (c - cc));
+                    if (dist <= radius * (0.7 + rnd.NextDouble() * 0.4))
+                        grid[r, c] = WATER;
+                }
+        }
+
+        private void Step()
+        {
+            double pGrow = trackGrow.Value / 1000.0;
+            double pFire = trackFire.Value / 1000.0;
+            double pWind = trackWind.Value / 100.0;
+
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    int state = grid[r, c];
+
+                    if (state == WATER)
+                        newGrid[r, c] = WATER;
+                    else if (state == BURNING)
+                        newGrid[r, c] = BURNED;
+                    else if (state == BURNED)
+                        newGrid[r, c] = EMPTY;
+                    else if (state == TREE)
+                    {
+                        if (HasBurningNeighbor(r, c, pWind))
+                            newGrid[r, c] = BURNING;
+                        else if (rnd.NextDouble() < pFire)
+                            newGrid[r, c] = BURNING;
+                        else
+                            newGrid[r, c] = TREE;
+                    }
+                    else // EMPTY
+                    {
+                        newGrid[r, c] = (rnd.NextDouble() < pGrow) ? TREE : EMPTY;
+                    }
+                }
+            }
+
+            Array.Copy(newGrid, grid, grid.Length);
+            DrawGrid();
+            UpdateStats();
+        }
+
+        private bool HasBurningNeighbor(int r, int c, double pWind)
+        {
+            int[] dr = { -1, 1, 0, 0 };
+            int[] dc = { 0, 0, -1, 1 };
+
+            for (int d = 0; d < 4; d++)
+            {
+                int nr = r + dr[d];
+                int nc = c + dc[d];
+
+                if (nr < 0 || nr >= rows || nc < 0 || nc >= cols) continue;
+                if (grid[nr, nc] == WATER) continue;
+                if (grid[nr, nc] == BURNING)
+                {
+                    if (d == 2) return true;
+                    if (rnd.NextDouble() > pWind) return true;
+                }
+            }
+            return false;
+        }
+
+        private void DrawGrid()
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(COLOR_EMPTY);
+                for (int r = 0; r < rows; r++)
+                    for (int c = 0; c < cols; c++)
+                    {
+                        Color color;
+                        switch (grid[r, c])
+                        {
+                            case TREE: color = COLOR_TREE; break;
+                            case BURNING: color = COLOR_BURNING; break;
+                            case BURNED: color = COLOR_BURNED; break;
+                            case WATER: color = COLOR_WATER; break;
+                            default: color = COLOR_EMPTY; break;
+                        }
+                        g.FillRectangle(new SolidBrush(color), c * CELL, r * CELL, CELL - 1, CELL - 1);
+                    }
+            }
+            pictureBox1.Image = bmp;
+        }
+
+        private void UpdateStats()
+        {
+            int trees = 0, burning = 0, burned = 0, empty = 0, water = 0;
+            for (int r = 0; r < rows; r++)
+                for (int c = 0; c < cols; c++)
+                    switch (grid[r, c])
+                    {
+                        case TREE: trees++; break;
+                        case BURNING: burning++; break;
+                        case BURNED: burned++; break;
+                        case WATER: water++; break;
+                        default: empty++; break;
+                    }
+            labStats.Text = string.Format(
+                "Деревьев: {0}    Горит: {1}    Сгорело: {2}    Вода: {3}    Пусто: {4}",
+                trees, burning, burned, water, empty);
+        }
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled) { timer1.Stop(); btStart.Text = "Старт"; }
+            else { timer1.Interval = trackSpeed.Value; timer1.Start(); btStart.Text = "Стоп"; }
+        }
+
+        private void btStep_Click(object sender, EventArgs e) { Step(); }
+
+        private void btReset_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            btStart.Text = "Старт";
+            InitGrid();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) { Step(); }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int c = e.X / CELL;
+            int r = e.Y / CELL;
+            if (r < 0 || r >= rows || c < 0 || c >= cols) return;
+
+            if (e.Button == MouseButtons.Left) grid[r, c] = BURNING;
+            else if (e.Button == MouseButtons.Right) grid[r, c] = WATER;
+            else if (e.Button == MouseButtons.Middle) grid[r, c] = TREE;
+
+            DrawGrid();
+            UpdateStats();
+        }
+
+        private void trackSpeed_Scroll(object sender, EventArgs e)
+        {
+            timer1.Interval = trackSpeed.Value;
+            labSpeed.Text = "Скорость: " + trackSpeed.Value + " мс";
+        }
+    }
+}
